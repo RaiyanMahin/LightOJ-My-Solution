2e8158606d56fd544b720632b63331b6b3181606
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2020-12-27 01:51:40 +0600

    Create Greetings from LightOJ.cpp

---
+++ Greetings from LightOJ.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    ll t ; cin >> t;
+    for(ll i = 1; i <= t; i++)
+    {
+        ll a , b; cin >> a >> b;
+        cout << "Case " << i << ": " << (a + b) << endl;
+
+    }
+}
+
